9ff840cc3dd81f75b879968f70d62dbd55a95f00
hanhly31231021229 <hanhly.31231021229@st.ueh.edu.vn>
2024-09-13 21:01:38 +0700

    Update Session05.cs

--- Session05.cs
+++ Session05.cs
@@ -14,7 +14,10 @@ namespace ConsoleApp1
             //baitap01();
             //baitap01b();
             //baitap02();
-            baitap03();
+            //baitap03();
+            //baitap04();            
+            //baitap05();
+            baitap06();
 
         }
         static void baitap01()
@@ -68,7 +71,7 @@ namespace ConsoleApp1
                 if (n == 0) return 1;
                 return n * factorial_recursion (n-1);
             }
-            
+
         }
         static void baitap03()
         {
@@ -91,20 +94,27 @@ namespace ConsoleApp1
             }
 
         }
-        static void baitap06()
+        static void baitap04()
 
         {
             int n = Convert.ToInt16(Console.ReadLine());
-
-            static void printPrimenumberundernumberN(int n)
+            printFirstNprimeNumber(n);
+            printPrimeNumberUnderN(n);
+            static bool isPrime(int number)            
             {
-                for (int i = 1; i <= n; i++)
-                {
-                    if (isPrime(i)) Console.WriteLine(i);
-                }
+                for (int i = 2; i <= number / 2; i++)
+                    if (number % i == 0)
+                        return false;
+                return true;
             }
-            
-            static void printFirstPrimenumber (int n)
+            static void printPrimeNumberUnderN(int n)
+            {
+                for(int i = 1;i<=n;i++)
+                    if(isPrime(i))
+                        Console.WriteLine(i);
+            }
+
+            static void printFirstNprimeNumber(int n)
             {
                 int count = 0;
                 int number = 1;
@@ -112,14 +122,59 @@ namespace ConsoleApp1
                 {
                     if (isPrime(number))
                     {
-                        Console.WriteLine(count);
+                        Console.WriteLine($"{count}: {number}");
                         count++;
                     }
                     number++;
-
+                }
+            }
+        }
+        static void baitap05()
+        {
+            int n = Convert.ToInt16(Console.ReadLine());
+            bool p1 = isPerfectnumber(n);
+            Console.WriteLine($"n = {n} is a perfect number: {p1}");
+            printPerfectNumberUnder1000(n);
+            static bool isPerfectnumber(int n)
+            { 
+                int a = 0;
+                for (int i = 1; i <= n / 2; i++)
+                {
+                    if (n % i == 0) a = a + i;
+                }
+                if (a == n) return true;
+                else return false; 
+            }
+            static void printPerfectNumberUnder1000(int n) 
+            {
+               for (int i = 1; i < 1000; i++)
+                {
+                    if (p1(i) == true) Console.WriteLine(i);
+                }
+            }
+        }
+        static void baitap06()
+        {
+            string s = Console.ReadLine();
+            s = s.ToLower();
+            bool check = checkPangram(s);
+            Console.WriteLine($"The string '{s}' is a pangram: {check}");
+            static bool checkPangram(string s)
+            {
+                bool [] check = new bool [26];
+                foreach (char c in s)
+                {
+                    for (char a = 'a'; a <= 'z'; a++)
+                    {
+                        if (c == a) check[c - 'a'] = true;
+                    }
+                }
+                foreach (bool b in check)
+                {
+                    if (b==false) return false;
+                    else return true;
                 }
             }
         }
     }
 }
-
