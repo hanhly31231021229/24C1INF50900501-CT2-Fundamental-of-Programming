c1d6174f42ed4296180cc9a86cd45e1f2270f37b
hanhly31231021229 <hanhly.31231021229@st.ueh.edu.vn>
2024-09-17 21:57:48 +0700

    Update SingleArray.cs

--- SingleArray.cs
+++ SingleArray.cs
@@ -92,6 +92,31 @@ namespace ConsoleApp1
       }
           return false;//khong co phan tu can bo
     }
+    ///Bubble sort
+    static void BubbleSort(int[] a)
+    {
+      int [] nums = new int[10];
+      for (int i = 0; i < nums.Length; i++)
+      {
+        nums[i] = Convert.ToInt32(Console.ReadLine());
+      }
+      for (int i = 0; i < nums.Length; i++)
+      {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+          if (nums[i] > nums[j])
+          {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+          }
+        }
+      }
+      foreach (int val in nums)
+      {
+        Console.Write($"{val} ");
+      }
+    }
     ///if the word appears in the phrase
     static bool CheckWordExistInSentence(string sentence, string word)
     {
